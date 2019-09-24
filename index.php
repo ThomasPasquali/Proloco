709388708a859f7ad6c167c2caed4b78da31b03e
unknown <tom.pasqui00@gmail.com>
2019-09-24 22:11:29 +0200

    Sito proloco bozza

---
+++ index.php
@@ -0,0 +1,43 @@
+<html>
+<head>
+    <title>Proloco Canale d'Agordo</title>
+    <link rel="stylesheet" href="bootstrap-4.3.1/css/bootstrap.min.css">
+    <script src="bootstrap-4.3.1/js/popper.min.js"></script>
+    <script src="bootstrap-4.3.1/js/jquery-3.4.1.min.js"></script>
+    <script src="bootstrap-4.3.1/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="css/index.css">
+</head>
+<body>
+    <div id="background-img">
+        <nav id="menu">
+            <div id="title">
+                <a href=""><img src="imgs/logo.jpg"></a>
+                <a href=""><h1>Proloco di<br>Canale d'Agordo</h1></a>
+            </div>
+            <ul class="nav justify-content-end">
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="" role="button" aria-haspopup="true" aria-expanded="false">Info</a>
+                    <div class="dropdown-menu">
+                        <a class="dropdown-item" href="">Appartamenti</a>
+                        <a class="dropdown-item" href="">Alberghi</a>
+                        <a class="dropdown-item" href="">Rifugi</a>
+                        <div class="dropdown-divider"></div>
+                        <a class="dropdown-item" href="">Booo</a>
+                    </div>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link active" href="">Eventi</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="">Contatti utili</a>
+                </li>
+
+            </ul>
+        </nav>
+        
+        <div id="content">
+        
+        </div>
+    </div>
+</body>
+</html>
