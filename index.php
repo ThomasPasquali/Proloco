2f340e346213e06edf96022be6bdb7af414ced47
ThomasPasquali <45013923+ThomasPasquali@users.noreply.github.com>
2020-02-24 21:06:57 +0100

    Modifiche estate-inverno

--- index.php
+++ index.php
@@ -143,17 +143,22 @@
         <div id="content-wrap">
             <div id="content">
 
-          <div id="home-estate" class="hidden subpage home">
-				<h1><?= $lang['estate_titolo_1'] ?></h1>
-				<p><?= $lang['estate_contenuto_1'] ?></p>
-				<h1><?= $lang['estate_titolo_2'] ?></h1>
-				<p><?= $lang['estate_contenuto_2'] ?></p>
-				<h1><?= $lang['estate_titolo_3'] ?></h1>
-				<p><?= $lang['estate_contenuto_3'] ?></p>
-				<h1><?= $lang['estate_titolo_4'] ?></h1>
-				<p><?= $lang['estate_contenuto_4'] ?></p>
+            <div id="home-estate" class="hidden subpage home">
+              <h1><?= $lang['estate_titolo_1'] ?></h1>
+              <h2><?= $lang['estate_sub_1_1'] ?></h2>
+              <h2><?= $lang['estate_sub_1_2'] ?></h2>
+              <h2><?= $lang['estate_sub_1_3'] ?></h2>
+              <h2><?= $lang['estate_sub_1_4'] ?></h2>
+            
+              <h1><?= $lang['estate_titolo_2'] ?></h1>
+              <h2><?= $lang['estate_sub_2_1'] ?></h2>
+              <h2><?= $lang['estate_sub_2_2'] ?></h2>
+              <h2><?= $lang['estate_sub_2_3'] ?></h2>
+              <h2><?= $lang['estate_sub_2_4'] ?></h2>
+              <h2><?= $lang['estate_sub_2_5'] ?></h2>
+              <h2><?= $lang['estate_sub_2_6'] ?></h2>
 				
-				<!-- #region Jssor Slider Start -->
+				      <!-- #region Jssor Slider Start -->
 	            <div id="jssor_estate" style="position:relative;margin:0 auto;top:0px;left:0px;width:650px;height:380px;overflow:hidden;visibility:hidden;">
 	              <!-- Loading Screen -->
 	              <div data-u="loading" class="jssorl-009-spin" style="position:absolute;top:0px;left:0px;width:100%;height:100%;text-align:center;background-color:rgba(0,0,0,0.7);">
@@ -162,7 +167,9 @@
 	              <div data-u="slides" style="cursor:default;position:relative;top:0px;left:0px;width:650px;height:380px;overflow:hidden;">
 	                  <div><img data-u="image" src="imgs/Home/Estate/1.JPG"/></div>
 	                  <div><img data-u="image" src="imgs/Home/Estate/2.JPG"/></div>
-	                  <div><img data-u="image" src="imgs/Home/Estate/3.JPG"/></div>
+                    <div><img data-u="image" src="imgs/Home/Estate/3.JPG"/></div>
+                    <div><img data-u="image" src="imgs/Home/Estate/4.JPG"/></div>
+                    <div><img data-u="image" src="imgs/Home/Estate/5.JPG"/></div>
 	              </div>
 	              <!-- Bullet Navigator -->
 	              <div data-u="navigator" class="jssorb051" style="position:absolute;bottom:12px;right:12px;" data-autocenter="1" data-scale="0.5" data-scale-bottom="0.75">
@@ -188,51 +195,52 @@
           </div>
           
           <div id="home-inverno" class="hidden subpage home">
-				<h1><?= $lang['inverno_titolo_1'] ?></h1>
-				<p><?= $lang['inverno_contenuto_1'] ?></p>
-				<h1><?= $lang['inverno_titolo_2'] ?></h1>
-				<p><?= $lang['inverno_contenuto_2'] ?></p>
-				<h1><?= $lang['inverno_titolo_3'] ?></h1>
-				<p><?= $lang['inverno_contenuto_3'] ?></p>
-				<h1><?= $lang['inverno_titolo_4'] ?></h1>
-				<p><?= $lang['inverno_contenuto_4'] ?></p>
-				<h1><?= $lang['inverno_titolo_5'] ?></h1>
-				<p><?= $lang['inverno_contenuto_5'] ?></p>
-				<h1><?= $lang['inverno_titolo_6'] ?></h1>
-				<p><?= $lang['inverno_contenuto_6'] ?></p>
+
+            <h1><?= $lang['inverno_titolo_1'] ?></h1>
+            <h2><?= $lang['inverno_sub_1_1'] ?></h2>
+            <h2><?= $lang['inverno_sub_1_2'] ?></h2>
+            <h2><?= $lang['inverno_sub_1_3'] ?></h2>
+          
+            <h1><?= $lang['inverno_titolo_2'] ?></h1>
+            <h2><?= $lang['inverno_sub_2_1'] ?></h2>
+            <h2><?= $lang['inverno_sub_2_2'] ?></h2>
+            <h2><?= $lang['inverno_sub_2_3'] ?></h2>
+            <h2><?= $lang['inverno_sub_2_4'] ?></h2>
 				
-				<!-- #region Jssor Slider Start -->
-	            <div id="jssor_inverno" style="position:relative;margin:0 auto;top:0px;left:0px;width:650px;height:380px;overflow:hidden;visibility:hidden;">
-	              <!-- Loading Screen -->
-	              <div data-u="loading" class="jssorl-009-spin" style="position:absolute;top:0px;left:0px;width:100%;height:100%;text-align:center;background-color:rgba(0,0,0,0.7);">
-	                  <img style="margin-top:-19px;position:relative;top:50%;width:38px;height:38px;" src="imgs/spin.svg" />
-	              </div>
-	              <div data-u="slides" style="cursor:default;position:relative;top:0px;left:0px;width:650px;height:380px;overflow:hidden;">
-	                  <div><img data-u="image" src="imgs/Home/Inverno/1.JPG"/></div>
-	                  <div><img data-u="image" src="imgs/Home/Inverno/2.JPG"/></div>
-	                  <div><img data-u="image" src="imgs/Home/Inverno/3.JPG"/></div>
-	              </div>
-	              <!-- Bullet Navigator -->
-	              <div data-u="navigator" class="jssorb051" style="position:absolute;bottom:12px;right:12px;" data-autocenter="1" data-scale="0.5" data-scale-bottom="0.75">
-	                  <div data-u="prototype" class="i" style="width:16px;height:16px;">
-	                      <svg viewbox="0 0 16000 16000" style="position:absolute;top:0;left:0;width:100%;height:100%;">
-	                          <circle class="b" cx="8000" cy="8000" r="5800"></circle>
-	                      </svg>
-	                  </div>
-	              </div>
-	              <!-- Arrow Navigator -->
-	              <div data-u="arrowleft" class="jssora051" style="width:55px;height:55px;top:0px;left:25px;" data-autocenter="2" data-scale="0.75" data-scale-left="0.75">
-	                  <svg viewbox="0 0 16000 16000" style="position:absolute;top:0;left:0;width:100%;height:100%;">
-	                      <polyline class="a" points="11040,1920 4960,8000 11040,14080 "></polyline>
-	                  </svg>
-	              </div>
-	              <div data-u="arrowright" class="jssora051" style="width:55px;height:55px;top:0px;right:25px;" data-autocenter="2" data-scale="0.75" data-scale-right="0.75">
-	                  <svg viewbox="0 0 16000 16000" style="position:absolute;top:0;left:0;width:100%;height:100%;">
-	                      <polyline class="a" points="4960,1920 11040,8000 4960,14080 "></polyline>
-	                  </svg>
-	              </div>
-	            </div>
-	            <!-- #endregion Jssor Slider End -->
+            <!-- #region Jssor Slider Start -->
+            <div id="jssor_inverno" style="position:relative;margin:0 auto;top:0px;left:0px;width:650px;height:380px;overflow:hidden;visibility:hidden;">
+              <!-- Loading Screen -->
+              <div data-u="loading" class="jssorl-009-spin" style="position:absolute;top:0px;left:0px;width:100%;height:100%;text-align:center;background-color:rgba(0,0,0,0.7);">
+                  <img style="margin-top:-19px;position:relative;top:50%;width:38px;height:38px;" src="imgs/spin.svg" />
+              </div>
+              <div data-u="slides" style="cursor:default;position:relative;top:0px;left:0px;width:650px;height:380px;overflow:hidden;">
+                  <div><img data-u="image" src="imgs/Home/Inverno/1.JPG"/></div>
+                  <div><img data-u="image" src="imgs/Home/Inverno/2.JPG"/></div>
+                  <div><img data-u="image" src="imgs/Home/Inverno/3.JPG"/></div>
+                  <div><img data-u="image" src="imgs/Home/Inverno/4.JPG"/></div>
+                  <div><img data-u="image" src="imgs/Home/Inverno/5.JPG"/></div>
+              </div>
+              <!-- Bullet Navigator -->
+              <div data-u="navigator" class="jssorb051" style="position:absolute;bottom:12px;right:12px;" data-autocenter="1" data-scale="0.5" data-scale-bottom="0.75">
+                  <div data-u="prototype" class="i" style="width:16px;height:16px;">
+                      <svg viewbox="0 0 16000 16000" style="position:absolute;top:0;left:0;width:100%;height:100%;">
+                          <circle class="b" cx="8000" cy="8000" r="5800"></circle>
+                      </svg>
+                  </div>
+              </div>
+              <!-- Arrow Navigator -->
+              <div data-u="arrowleft" class="jssora051" style="width:55px;height:55px;top:0px;left:25px;" data-autocenter="2" data-scale="0.75" data-scale-left="0.75">
+                  <svg viewbox="0 0 16000 16000" style="position:absolute;top:0;left:0;width:100%;height:100%;">
+                      <polyline class="a" points="11040,1920 4960,8000 11040,14080 "></polyline>
+                  </svg>
+              </div>
+              <div data-u="arrowright" class="jssora051" style="width:55px;height:55px;top:0px;right:25px;" data-autocenter="2" data-scale="0.75" data-scale-right="0.75">
+                  <svg viewbox="0 0 16000 16000" style="position:absolute;top:0;left:0;width:100%;height:100%;">
+                      <polyline class="a" points="4960,1920 11040,8000 4960,14080 "></polyline>
+                  </svg>
+              </div>
+            </div>
+            <!-- #endregion Jssor Slider End -->
           </div>
           
           <div id="estate-inverno">
@@ -484,6 +492,12 @@
                 <h2 data-toggle="tooltip" data-placement="bottom" title="<?= $lang['fino_a_Belluno'] ?>">
                   <a href="https://www.trenitalia.com" target="_blank">
                     <?= $lang['orari_trenitalia'] ?>
+                  </a>
+                </h2>
+
+                <h2 data-toggle="tooltip" data-placement="bottom" title="<?= $lang['da_per_venezia'] ?>">
+                  <a href="files/brusutti.pdf" target="_blank">
+                    <?= $lang['brusutti'] ?>
                   </a>
                 </h2>
 
